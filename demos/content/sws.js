c0235b7c2e7e90fe29bbb9205815ad25ae9e03cb
tom zhou <zs68j2ee@gmail.com>
2014-11-15 12:32:31 +0800

    fixed typo in broswer sws.js

--- demos/content/sws.js
+++ demos/content/sws.js
@@ -742,4 +742,4 @@
 	Export.Uint8ToBuffer = Uint8ToBuffer;
 })(typeof module  !== 'undefined' ? module.exports                    :(window.sws = window.sws || {}), 
    typeof require !== 'undefined' ? require('tweetnacl/nacl-fast.js') : window.nacl,
-   typeof require !== 'undefined' ? require('ws')                     : window.WebSocket);
+   typeof require !== 'undefined' ? require('wspp')                   : window.WebSocket);
