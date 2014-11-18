6a7c4317fe29dcda7b82bdb2899f735c16c9cff0
tom zhou <zs68j2ee@gmail.com>
2014-11-18 08:48:21 +0800

    added default secinfo when create secure websocket/server

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ module.exports.Server = require('./SecureWebSocketServer');
 module.exports.Stream = require('./SecureStream');
 
 module.exports.createServer = function (options, connectionListener) {
-  var server = new module.exports.Server(options, options.secinfo || options);
-  if (typeof connectionListener === 'function') {
-    server.on('connection', connectionListener);
-  }
-  return server;
+	var kp;
+	// check on secinfo
+	if (!(options.secinfo && typeof options.secinfo === 'object')) {
+		kp = sws.keyPair();
+		options.secinfo = {myPublicKey: kp.publicKey, mySecretKey: kp.mySecretKey};
+	}
+
+	var server = new module.exports.Server(options, options.secinfo);
+	if (typeof connectionListener === 'function') {
+		server.on('connection', connectionListener);
+	}
+	return server;
 };
 
 module.exports.connect = module.exports.createConnection = function (address, secinfo, openListener) {
-  var client = new module.exports(address, secinfo);
-  if (typeof openListener === 'function') {
-    client.on('open', openListener);
-  }
-  return client;
+	var kp;
+	// check on secinfo
+	if (secinfo && typeof secinfo === 'function') {
+		openListener = secinfo;
+
+		kp = sws.keyPair();
+		secinfo = {myPublicKey: kp.publicKey, mySecretKey: kp.mySecretKey};
+	} else if (!(secinfo && typeof secinfo === 'object')) {
+		kp = sws.keyPair();
+		secinfo = {myPublicKey: kp.publicKey, mySecretKey: kp.mySecretKey};
+	}
+
+	var client = new module.exports(address, secinfo);
+	if (typeof openListener === 'function') {
+		client.on('open', openListener);
+	}
+	return client;
 };
 
 // NACL
